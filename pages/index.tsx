3a05d5b98d8063fe1a93b5f380710a190dd1661f
brlaney42 <brlaney@outlook.com>
2021-07-09 20:52:18 -0500

    index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import styles from '../styles/Home.module.scss'
 import Container from '@material-ui/core/Container'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
@@ -6,6 +5,7 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardActions from '@material-ui/core/CardActions'
 import Grid from '@material-ui/core/Grid'
+import styles from '@/styles/Home.module.scss'
 
 export default function Home() {
   return (
